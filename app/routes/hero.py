8e97009ef08ecb192566ebd19c39c1472501de3b
soufiane92 <soufiane.cherkaoui1992@gmail.com>
2025-03-28 10:50:22 +0100

    [routes] add routes to api endpoints

---
+++ app/routes/hero.py
@@ -0,0 +1,98 @@
+from typing import Annotated
+from fastapi import APIRouter, Query, status, HTTPException
+from app.db.database import SessionDep
+from app.schemas.hero import (
+    HeroCreate,
+    HeroPublic,
+    HeroUpdate,
+)
+from app.crud.hero import (
+    create_hero,
+    read_heroes,
+    read_hero,
+    update_hero,
+    delete_hero,
+)
+
+hero_router = APIRouter(prefix="/heroes", tags=["Heroes"])
+
+
+@hero_router.post(
+    "/",
+    response_model=HeroPublic,
+    status_code=status.HTTP_201_CREATED,
+)
+def add_new_hero(
+    db_session: SessionDep,
+    hero: HeroCreate,
+):
+    db_hero = create_hero(db_session, hero)
+    return db_hero
+
+
+@hero_router.get(
+    "/",
+    response_model=list[HeroPublic],
+    status_code=status.HTTP_200_OK,
+)
+def list_all_heroes(
+    db_session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+):
+    db_heroes = read_heroes(db_session, offset, limit)
+    return db_heroes
+
+
+@hero_router.get(
+    "/{hero_id}",
+    response_model=HeroPublic,
+    status_code=status.HTTP_200_OK,
+)
+def get_hero_by_id(
+    db_session: SessionDep,
+    hero_id: int,
+):
+    db_hero = read_hero(db_session, hero_id)
+    if not db_hero:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Hero with ID {hero_id} not found",
+        )
+    return db_hero
+
+
+@hero_router.patch(
+    "/{hero_id}",
+    response_model=HeroPublic,
+    status_code=status.HTTP_202_ACCEPTED,
+)
+def update_existing_hero(
+    db_session: SessionDep,
+    hero_id: int,
+    updated_hero: HeroUpdate,
+):
+    db_hero = update_hero(db_session, hero_id, updated_hero)
+    if not db_hero:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Hero with ID {hero_id} not found",
+        )
+    return db_hero
+
+
+@hero_router.delete(
+    "/{hero_id}",
+    status_code=status.HTTP_202_ACCEPTED,
+)
+def delete_existing_hero(
+    db_session: SessionDep,
+    hero_id: int,
+) -> dict:
+    message = delete_hero(db_session, hero_id)
+    if not message:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Hero with ID {hero_id} not found",
+        )
+    return message
---
+++ app/routes/team.py
@@ -0,0 +1,99 @@
+from typing import Annotated
+from fastapi import APIRouter, Query, status, HTTPException
+from app.db.database import SessionDep
+from app.schemas.team import (
+    TeamCreate,
+    TeamPublic,
+    TeamUpdate,
+)
+from app.crud.team import (
+    create_team,
+    read_teams,
+    read_team,
+    update_team,
+    delete_team,
+)
+
+
+team_router = APIRouter(prefix="/teams", tags=["Teams"])
+
+
+@team_router.post(
+    "/",
+    response_model=TeamPublic,
+    status_code=status.HTTP_201_CREATED,
+)
+def add_new_team(
+    db_session: SessionDep,
+    team: TeamCreate,
+):
+    db_team = create_team(db_session, team)
+    return db_team
+
+
+@team_router.get(
+    "/",
+    response_model=list[TeamPublic],
+    status_code=status.HTTP_200_OK,
+)
+def list_all_teams(
+    db_session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+):
+    db_teams = read_teams(db_session, offset, limit)
+    return db_teams
+
+
+@team_router.get(
+    "/{team_id}",
+    response_model=TeamPublic,
+    status_code=status.HTTP_200_OK,
+)
+def get_team_by_id(
+    db_session: SessionDep,
+    team_id: int,
+):
+    db_team = read_team(db_session, team_id)
+    if not db_team:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Team with ID {team_id} not found",
+        )
+    return db_team
+
+
+@team_router.patch(
+    "/{team_id}",
+    response_model=TeamPublic,
+    status_code=status.HTTP_202_ACCEPTED,
+)
+def update_existing_team(
+    db_session: SessionDep,
+    team_id: int,
+    updated_team: TeamUpdate,
+):
+    db_team = update_team(db_session, team_id, updated_team)
+    if not db_team:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Team with ID {team_id} not found",
+        )
+    return db_team
+
+
+@team_router.delete(
+    "/{team_id}",
+    status_code=status.HTTP_202_ACCEPTED,
+)
+def delete_existing_team(
+    db_session: SessionDep,
+    team_id: int,
+) -> dict:
+    message = delete_team(db_session, team_id)
+    if not message:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Team with ID {team_id} not found",
+        )
+    return message
